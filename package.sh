89544035c3d10c071450fe2a1fc8d61164e672cf
nobodyindustries <hello@nobodyownsthisdomain.org>
2021-04-17 15:26:29 +0300

    Changed debug command

--- package.sh
+++ package.sh
@@ -8,7 +8,7 @@ fi
 if [ "$1" == "d" ]
 then
     echo "= DEBUG ="
-	cc -std=c11 -DDEBUG -Wall -Wpedantic -Wshadow -Wextra -Werror=implicit-int -Werror=incompatible-pointer-types -Werror=int-conversion -Wvla -g -Og -fsanitize=address -fsanitize=undefined main.c `sdl2-config --cflags` `sdl2-config --libs`
+	cc -std=c11 -o grid -DDEBUG -Wall -Wpedantic -Wshadow -Wextra -Werror=implicit-int -Werror=incompatible-pointer-types -Werror=int-conversion -Wvla -g -Og -fsanitize=address -fsanitize=undefined main.c `sdl2-config --cflags` `sdl2-config --libs`
 	echo "Size: $(du -skh ./grid)"
 fi
 if [ "$1" == "f" ]
